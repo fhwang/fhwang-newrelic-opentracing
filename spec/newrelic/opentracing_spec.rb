29e2f5f6d786d35614c8fd8df5a1552078c70fa1
Francis Hwang <sera@fhwang.net>
2020-01-06 15:56:33 -0500

    Fix specs.

--- spec/newrelic/opentracing_spec.rb
+++ spec/newrelic/opentracing_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe NewRelic::OpenTracing do
   it "has a version number" do
     expect(NewRelic::OpenTracing::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-require "fhwang/newrelic/opentracing"
+require "newrelic/opentracing"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
